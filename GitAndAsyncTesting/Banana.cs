90cf61be404df552cff61f09699e81a83899fd7c
Nicklas Stanislaus <99009841+Nicklas-Stanislaus@users.noreply.github.com>
2025-02-16 17:38:04 +0100

    Created banana class and added text line

---
+++ GitAndAsyncTesting/Banana.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitAndAsyncTesting
+{
+    class Banana
+    {
+        private int Weight = 2; 
+
+    }
+}
--- GitAndAsyncTesting/Program.cs
+++ GitAndAsyncTesting/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Small changes!");
+            Console.WriteLine("this now has a banana");
             Console.WriteLine("This was in a new branch");
         }
     }
